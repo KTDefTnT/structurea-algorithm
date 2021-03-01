3fede2830e1dbb90528e3658187764d86c7d3507
胡露 <hul@elimen.com.cn>
2021-03-01 22:27:47 +0800

    栈

---
+++ Stack/calc_exp.js
@@ -0,0 +1,35 @@
+const { Stack } = require('./index.js');
+/**
+ * 1、碰到数字则压入栈内
+ * 2、碰到符号 连续取出两个数字进行计算的到 res，第二个字符 操作第一个字符
+ * 3、将res压入栈内
+ * 直到整个计算完毕后 弹出栈内元素则为结果
+ */
+function calc_exp(exp) {
+  let exp_sign = ['+', '-', '*', '/'];
+  var stack = new Stack();
+  let first_value;
+  let last_value;
+  let res;
+  for(let i = 0; i < exp.length; i++) {
+    if (exp_sign.includes(exp[i])) {
+      // 取出两个栈内元素
+      last_value = stack.pop();
+      first_value = stack.pop();
+      res = Math.floor(eval(`${first_value}${exp[i]}${last_value}`));
+      stack.push(res);
+    } else {
+      stack.push(exp[i]);
+    }
+  }
+
+  return stack.top();
+}
+
+// ["4", "13", "5", "/", "+"] 等价于(4 + (13 / 5)) = 6
+// ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"] 
+// 等价于((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+var arr1 = ["4", "13", "5", "/", "+"];
+var arr2 = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"];
+console.log('结果1:', calc_exp(arr1));
+console.log('结果2:', calc_exp(arr2));
---
+++ Stack/min_stack.js
@@ -0,0 +1,44 @@
+/**
+ * 实现一个栈，除了常见的push，pop方法以外，提供一个min方法，返回栈里最小的元素，且时间复杂度为o(1) - 栈顶就是最小值
+ * 
+ * 
+ */
+const { Stack } = require('./index.js');
+
+function MinStack() {
+  var data_stack = new Stack(); // 存储数据
+  var min_stack = new Stack(); // 存储每次数据操作的最小值
+
+  this.push = function(item) {
+    data_stack.push(item);
+
+    // 先判断min_stack中是否有数据，若无数据则直接插入
+    if (min_stack.isEmpty() || min_stack.top() > item) {
+      min_stack.push(item);
+    } else {
+      // 若插入的数据大于当前已存在的最小值 则将最小值重新插入一次
+      min_stack.push(min_stack.top());
+    }
+  }
+
+  // 弹出栈顶元素
+  this.pop = function() {
+    min_stack.pop(); // 同步弹出当前最小值
+    return data_stack.pop();
+  }
+
+  // 获取最小值
+  this.min = function() {
+    return min_stack.top();
+  }
+}
+
+minStack = new MinStack();
+
+minStack.push(3);
+minStack.push(6);
+minStack.push(1);
+console.log(minStack.min());
+minStack.push(2);
+minStack.push(8);
+console.log(minStack.min());
