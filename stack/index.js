191c437f492d76f18be713c366367c769fd6364b
胡露 <hul@elimen.com.cn>
2021-03-02 07:58:23 +0800

    栈操作

--- stack/index.js
+++ stack/index.js
@@ -1,48 +1,38 @@
-/**
- * 栈有以下几个方法：
- *  push 添加一个元素到栈顶（向桶里放入一个羽毛球）
- *  pop 弹出栈顶元素（从桶里拿出一个羽毛球）
- *  top 返回栈顶元素，注意，不是弹出（看一眼桶里最顶端的羽毛球，但是不拿）
- *  isEmpty 判断栈是否为空（看看羽毛球是不是都用完了）
- *  size 返回栈里元素的个数（数一下桶里还有多少羽毛球）
- *  clear 清空栈（把桶里的羽毛球都倒出来扔掉）
- */
+// 栈 先进后出 first in last out
 class Stack {
-  constructor () {
-    this.stack = []; 
+  constructor() {
+    this.items = []; // 存储数据
   }
-  
-  /** 添加元素 */
+  // 从栈顶添加元素， 也叫压栈 -- 无返回值，改变原数据
   push(item) {
-    this.stack.push(item);
+    this.items.push(item);
+  }
+  // 从栈顶弹出元素 出栈-- 返回值 -栈顶元素，改变原数据
+  pop(item) {
+    return this.items.pop(item);
   }
 
-  /** 弹出元素 */
-  pop() {
-    return this.stack.pop();
-  }
-
-  /** top返回栈顶元素 */
+  // 返回栈顶元素 弹出 -- 返回值 - 栈顶元素
   top() {
-    return this.stack[stack.length - 1];
+    return this.items[this.items.length - 1];
   }
 
-  /** isEmpty判断栈是否为空 */
+  // 判断栈是否为空
   isEmpty() {
-    return this.stack.length === 0;
+    return this.items.length === 0;
   }
 
-  /** size 返回栈里元素的个数 */
+  // 返回栈的大小
   size() {
-    return this.stack.length;
+    return this.items.length;
   }
 
-  /** clear 清空栈 */
+  // 清空栈
   clear() {
-    this.stack = [];
+    this.items = [];
   }
 }
 
 module.exports = {
   Stack
-}
+};
